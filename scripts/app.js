084ac29dc5ea58090b7ec6220522b0d9ee0d6ecb
miau715 <miau715@gmail.com>
2020-10-17 11:40:31 +0800

    init

---
+++ scripts/app.js
@@ -0,0 +1,101 @@
+$.getJSON("./data/vocabulary.json", function(json) {
+  const vObj = json;
+  const vList = $('#vocabulary-list');
+  const vLength = vObj.vocabulary.length;
+  const vNumber = 5;
+  let isPOJ = true;
+  $('input[name=use-poj]').on('click', function() {
+    console.log($(this).attr('id'));
+    if ($('input[name=use-poj]:checked').attr('id') === 'use-tl') {
+      $('.poj').addClass('hide');
+      $('.tl').addClass('show');
+    }
+    else {
+      $('.poj').removeClass('hide');
+      $('.tl').removeClass('show');
+    }
+  });
+
+  // generate 10 random numbers 
+  const numArray = [...Array(vLength).keys()];
+  const randomNumArray = (arr, length) => {
+    let newArr = arr;
+    for (i = 0; i < length; i++) {
+      let randomI = Math.floor(Math.random()*(arr.length));
+      let itemAtI = newArr[randomI];
+
+      newArr[randomI] = newArr[i];
+      newArr[i] = itemAtI;
+    }
+    return newArr.slice(0, length);;
+  }
+
+  const randomIArray = randomNumArray(numArray, vNumber);
+
+  let audioArray = [];
+
+  randomIArray.forEach(function(number, i) {
+    let getVocabulary = vObj.vocabulary[number]["v"];
+    vList.append(`<li><div class="result-wrapper"><div class="result"><span class="han">${getVocabulary}</span></div><div class="link"><a href="https://www.moedict.tw/'${getVocabulary}" target="_blank"><span class="poj">Khòaⁿ jī-tián</span><span class="tl">Khuànn jī-tián</span><span class="han">看字典</span></a></div></div></li>`);
+    $.ajax({
+      url: `https://www.moedict.tw/t/${getVocabulary}.json`,
+      dataType: "json",
+      success: function(result) {
+        let thisVObj = result;
+        let thisVTTL = result.h[0].T;
+        let thisVTPOJ = thisVTTL.replace(/(o)([^.!?,\w\s\u2011]*)o/ig, '$1$2\u0358')
+                        .replace(/ts/g, 'ch')
+                        .replace(/Ts/g, 'Ch')
+                        .replace(/u([^‑-\w\s]*)a/g, 'o$1a')
+                        .replace(/u([^‑-\w\s]*)e/g, 'o$1e')
+                        .replace(/i([^‑-\w\s]*)k($|[-\s])/g, 'e$1k$2')
+                        .replace(/i([^‑-\w\s]*)ng/g, 'e$1ng')
+                        .replace(/nn($|[‑-\s])/g, 'ⁿ$1')
+                        .replace(/nnh($|[‑-\s])/g, 'hⁿ$1')
+                        .replace(/([ie])r/g, '$1\u0358')
+                        .replace(/\u030B/g, "\u0306"); // 9th tone
+        let thisVAudio = thisVObj.h[0]._;
+        if (Number(thisVAudio) < 20000 || Number(thisVAudio) > 50000) {
+          thisVAudio = (100000 + Number(thisVAudio)).toString().replace(/^1/, '');
+        }
+        audioArray[i] = thisVAudio;
+        vList.find('li').eq(i).find(".result").prepend(`<span class="poj">${thisVTPOJ}</span><span class="tl">${thisVTTL}</span>`);
+      }
+    });
+  });
+  $("#start-test").on('click', function(){
+    const repeat = 3;
+    let playAudio = function(currentAudioI) {
+      let vAudio = new Audio(`http://t.moedict.tw/${audioArray[currentAudioI]}.ogg`);
+      let playTimes = 0;
+
+      if (currentAudioI === 0 && playTimes === 0) {
+        vAudio.play();
+        playTimes++;
+      }
+      else {
+        setTimeout(function(){
+          vAudio.play();
+          playTimes++;
+        }, 3000);
+      }
+
+      vAudio.addEventListener('ended', function() {
+        vAudio.currentTime = 0;
+        if (playTimes <= repeat - 1) {
+          setTimeout(function(){
+            vAudio.play();
+            playTimes++;
+          }, 3000);
+        }
+        else {
+          if (currentAudioI < audioArray.length - 1) {
+            currentAudioI++;
+            playAudio(currentAudioI);
+          }
+        }
+      }, false);
+    }
+    playAudio(0);
+  });
+});
